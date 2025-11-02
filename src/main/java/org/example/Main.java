ee49ddd1d22065f980cfa01e0078c49787321402
Ahmetberkant <ahmetberkanttulu@yaani.com>
2025-11-02 23:32:10 +0300

    tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 20);
+        Wall wall = new Wall(5, 4);
+
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("Lastname: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Is teen? " + person.isTeen());
+
+        System.out.println("Wall width: " + wall.getWidth());
+        System.out.println("Wall height: " + wall.getHeight());
+        System.out.println("Wall area: " + wall.getArea());
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String email;
+    private String phoneNumber;
+
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    public Person(String firstName, String lastName, int age,
+                  String gender, String email, String phoneNumber) {
+        this(firstName, lastName, age); // chaining
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+    public boolean isTeen() { return age >= 13 && age <= 19; }
+}
